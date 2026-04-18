285e57680c5433b19c72e1b749abe72a557141f8
sakshikurbet <sakshikurbet@gmail.com>
2026-04-18 12:32:07 +0530

    Update DBConnection.java

--- src/main/java/com/mycompany/resumeanalyse/DBConnection.java
+++ src/main/java/com/mycompany/resumeanalyse/DBConnection.java
@@ -11,23 +11,22 @@ import java.sql.SQLException;
 public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/resume_analysis_db";
-    private static final String USER = "root"; // or 'resume_user' if you created one
-    private static final String PASSWORD = "Sakshi@2005"; // ← replace with your MySQL password
+    private static final String USER = "root"; 
+    private static final String PASSWORD = ""; // ← replace with your MySQL password
 
     // Get connection method
     public static Connection getConnection() {
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("✅ Database connected successfully!");
+            System.out.println("Database connected successfully!");
         } catch (SQLException e) {
-            System.out.println("❌ Database connection failed!");
+            System.out.println("Database connection failed!");
             e.printStackTrace();
         }
         return conn;
     }
 
-    // Quick test
     public static void main(String[] args) {
         getConnection();
     }
